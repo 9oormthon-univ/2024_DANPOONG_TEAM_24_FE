73e2d3c092568e92bfb40fe7d8bcc7584aa49bc7
Lee Jihee <129031670+munjji@users.noreply.github.com>
2024-11-19 22:15:34 +0900

    fix: 커뮤니티 페이지 css 수정

--- src/pages/Community/Community.tsx
+++ src/pages/Community/Community.tsx
@@ -5,6 +5,16 @@ import ScrollCategoryBar from '../../components/Community/ScrollCategoryBar'
 import Contents from '../../components/Community/Contents'
 import WriteButton from '../../components/Community/WriteButton'
 
+const comments = [
+  { nickname: '익명의 카피바라', updateHour: 1, postId: 1 },
+  { nickname: '익명의 고양이', updateHour: 2, postId: 2 },
+  { nickname: '익명의 고양이', updateHour: 2, postId: 3 },
+  { nickname: '익명의 고양이', updateHour: 2, postId: 4 },
+  { nickname: '익명의 고양이', updateHour: 2, postId: 5 },
+  { nickname: '익명의 고양이', updateHour: 2, postId: 6 },
+  { nickname: '익명의 고양이', updateHour: 2, postId: 7 },
+]
+
 const Community: React.FC = () => {
   useEffect(() => {
     localStorage.setItem('category', '전체')
@@ -14,19 +24,24 @@ const Community: React.FC = () => {
     <div className="flex flex-col items-center">
       <Header />
       <Footer />
-      <div className="flex flex-col border-t border-t-200 mt-[10px] items-center">
-        <ScrollCategoryBar />
-        {/* map 함수를 사용하여 나타낼 예정입니다! */}
-        <div className="flex flex-col gap-[10px]">
-          <Contents
-            nickname="익명의 카피바라"
-            updateHour={1}
-            postId={1}
-            isLabel
-          />
-        </div>
-        <div className="fixed right-[16px] bottom-[108px] z-50">
-          <WriteButton />
+      <div className="relative w-[390px] flex flex-col items-center">
+        <div className="flex flex-col w-full border-t border-t-200 mt-[10px] items-center">
+          <ScrollCategoryBar />
+          <div className="flex flex-col gap-[10px]">
+            {comments.map((comment, index) => (
+              <Contents
+                key={comment.postId}
+                nickname={comment.nickname}
+                updateHour={comment.updateHour}
+                postId={comment.postId}
+                isLabel
+                isLastComment={index === comments.length - 1}
+              />
+            ))}
+          </div>
+          <div className="fixed right-4 bottom-[108px] z-50">
+            <WriteButton />
+          </div>
         </div>
       </div>
     </div>
