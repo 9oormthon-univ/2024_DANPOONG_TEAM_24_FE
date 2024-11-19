b3f7eeb0be255b01c6ba45374cd30bd046d5da73
gmlwlsdl <heejin094@naver.com>
2024-11-19 13:01:17 +0900

    feat: 메인 페이지 생성

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+import Main from './pages/Main/Main.tsx'
 import Information from './pages/Information.tsx'
 import Address from './pages/Main/Address.tsx'
 import Community from './pages/Community/Community.tsx'
 import Write from './pages/Community/Write.tsx'
 
 const router = createBrowserRouter([
-  // 정보 페이지
+  { path: '/', element: <Main /> },
   { path: '/information', element: <Information /> },
   { path: '/address', element: <Address /> },
   { path: '/community', element: <Community /> },
