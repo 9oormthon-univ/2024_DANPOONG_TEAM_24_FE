09c56847534352170b481c40ac77c77067287f9a
gmlwlsdl <heejin094@naver.com>
2024-11-20 23:06:54 +0900

    feat: 헤더에 위치한 로고 클릭시 메인 페이지로 리다이렉트

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -35,7 +35,10 @@ function Header() {
     <>
       <header className="mt-7 ml-[17px] mr-[19px] w-[390px]">
         <div className="flex px-4 gap-[94px]">
-          <div className="flex flex-row gap-[10px] items-center">
+          <div
+            className="flex flex-row gap-[10px] items-center"
+            onClick={() => navigation('/')}
+          >
             <img src={logo} alt="logoIcon" className="w-9 h-9" />
             <img src={logoText} alt="logoText" className="w-[37.33px] h-7" />
           </div>
