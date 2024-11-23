2d71655ade4bacf9efd4d5a0cad5e4fd4031571e
Lee Jihee <129031670+munjji@users.noreply.github.com>
2024-11-24 03:34:18 +0900

    fix: 정부 추천 편의점 건강 레시피 슬라이더 시간 조절

--- src/components/Main/BackRecipeCarouselSlider.tsx
+++ src/components/Main/BackRecipeCarouselSlider.tsx
@@ -20,7 +20,7 @@ export default class BackRecipeCarouselSlider extends Component<BackRecipeCarous
       slidesToShow: 2,
       slidesToScroll: 1,
       autoplay: true,
-      autoplaySpeed: 5000,
+      autoplaySpeed: 2000,
       cssEase: 'linear',
       variableWidth: true,
     }
