ec55d94f9b7e0027cba14484085d0c064359e3d3
Lee Jihee <129031670+munjji@users.noreply.github.com>
2024-11-27 02:36:21 +0900

    fix: NoContents 컴포넌트 수정

--- src/components/Community/NoContents.tsx
+++ src/components/Community/NoContents.tsx
@@ -1,12 +1,32 @@
 import React from 'react'
 import ieum from '../../assets/splash/sad_ieum.svg'
 
-const subjects = {
-  my_post: '앗 작성된 글이 없어요!',
-  commented: '댓글을 단 게시글이\n 없어요!',
-  liked: '좋아요 누른 글이\n 없어요!',
-  store: '앗 1km 내에 \n존재하는 가맹점이 없어요!',
-  address: '앗 설정된 주소가 없어요!',
+interface TextInterface {
+  main: string
+  sub: string
+}
+
+const subjects: Record<string, TextInterface> = {
+  my_post: {
+    main: '앗 작성된 글이 없어요!',
+    sub: '커뮤니티에 레시피를 공유해보세요',
+  },
+  commented: {
+    main: '앗 댓글을 단 글이 없어요!',
+    sub: '커뮤니티 글에 의견을 남겨보세요',
+  },
+  liked: {
+    main: '앗 좋아요 누른 글이 없어요!',
+    sub: '커뮤니티 글에 공감해보세요',
+  },
+  store: {
+    main: '앗 1km 내에 존재하는 가맹점이 없어요!',
+    sub: '위치를 다시 설정해주세요',
+  },
+  address: {
+    main: '앗 설정된 주소가 없어요!',
+    sub: '위치를 다시 설정해주세요',
+  },
 }
 
 const NoContents: React.FC<{ subjectKey?: keyof typeof subjects }> = ({
@@ -14,9 +34,12 @@ const NoContents: React.FC<{ subjectKey?: keyof typeof subjects }> = ({
 }) => {
   return (
     <div className="flex justify-center items-center bg-white">
-      <div className="flex flex-col items-center justify-center gap-[61px] mt-[148px]">
-        <p className="font-B00 text-[26px] text-center text-800 leading-140 font-medium whitespace-pre-wrap">
-          {subjectKey ? subjects[subjectKey] : ''}
+      <div className="flex flex-col items-center justify-center gap-1 mt-[148px] ">
+        <p className="font-B00 text-[22px] text-center text-800 leading-140 font-medium whitespace-pre-wrap">
+          {subjectKey ? subjects[subjectKey].main : ''}
+        </p>
+        <p className="font-M00 text-base text-center text-400 leading-135 font-normal mb-12">
+          {subjectKey ? subjects[subjectKey].sub : ''}
         </p>
         <img src={ieum} alt="이음_프로필" />
       </div>
