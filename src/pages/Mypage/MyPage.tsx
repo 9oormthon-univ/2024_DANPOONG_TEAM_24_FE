56624c71795165bd050d06fba95c7a65b000439c
Lee Jihee <129031670+munjji@users.noreply.github.com>
2024-11-27 02:26:25 +0900

    fix: 마이페이지 아이콘 추가

--- src/pages/Mypage/MyPage.tsx
+++ src/pages/Mypage/MyPage.tsx
@@ -1,10 +1,14 @@
 import { useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
 import Post from '../../assets/mypage/Post.svg'
 import Comment from '../../assets/mypage/Comment.svg'
 import Like from '../../assets/mypage/Like.svg'
 import Xbutton from '../../assets/mypage/Xbutton.svg'
 import useMypage from '../../hooks/MyPage/useMyPage'
-import { useEffect } from 'react'
+import Assignment from '../../assets/mypage/assignment.svg'
+import Service from '../../assets/mypage/document.svg'
+import Privacy from '../../assets/mypage/privacy.svg'
+import Logout from '../../assets/mypage/logout.svg'
 
 interface SvgItem {
   src: string
@@ -12,6 +16,11 @@ interface SvgItem {
   description: string
   path: string
   category: string
+}
+
+interface BottomSvgItem {
+  src: string
+  subject: string
 }
 
 function MyPage() {
@@ -39,6 +48,21 @@ function MyPage() {
       description: '좋아요 누른 글',
       path: '/mypage/likepost',
       category: 'liked',
+    },
+  ]
+
+  const bottomSvgs: BottomSvgItem[] = [
+    {
+      src: Assignment,
+      subject: '계정 정보',
+    },
+    {
+      src: Service,
+      subject: '서비스 이용약관',
+    },
+    {
+      src: Privacy,
+      subject: '개인정보 처리 방침',
     },
   ]
 
@@ -97,21 +121,21 @@ function MyPage() {
         </div>
 
         <div className="px-4">
-          {['계정 정보', '서비스 이용약관', '개인정보처리 방침'].map(
-            (text, index) => (
-              <div
-                key={index}
-                className="py-3 border-b border-200"
-                onClick={handleClick}
-              >
-                <a className="font-M00 text-m">{text}</a>
-              </div>
-            )
-          )}
+          {bottomSvgs.map((svg, index) => (
+            <div
+              key={index}
+              className="flex items-center justify-start gap-4 py-3 border-b border-200 text-800"
+              onClick={handleClick}
+            >
+              <img src={svg.src} />
+              <a className="font-M00 text-m">{svg.subject}</a>
+            </div>
+          ))}
           <div
-            className="py-3 border-b border-200 cursor-pointer"
+            className="flex items-center justify-start gap-4 py-3 border-b border-200 cursor-pointer"
             onClick={handleLogout}
           >
+            <img src={Logout} alt="로그아웃" />
             <span className="font-M00 text-m">로그아웃</span>
           </div>
         </div>
