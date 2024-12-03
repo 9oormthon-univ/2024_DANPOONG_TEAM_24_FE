7b66996e0ca1f957a2f5d4c7820bdb4ccd0c5660
Lee Jihee <129031670+munjji@users.noreply.github.com>
2024-12-03 16:52:22 +0900

    fix: 카카오맵 카테고리 수정 및 Filter 변경

--- src/components/Around/Filter.tsx
+++ src/components/Around/Filter.tsx
@@ -20,7 +20,7 @@ interface Filter {
 // 24/11/22 희진 category_id 추가
 const Filter: Filter[] = [
   { id: 1, label: '전체', category_id: 3, image: all },
-  { id: 2, label: '검증된 맛집', category_id: 11, image: verified },
+  { id: 2, label: '검증된 맛집', category_id: 12, image: verified },
   { id: 3, label: '한식', category_id: 1, image: korean },
   { id: 4, label: '중식', category_id: 8, image: chinese },
   { id: 5, label: '양식', category_id: 9, image: western },
@@ -29,7 +29,8 @@ const Filter: Filter[] = [
   { id: 8, label: '패스트푸드', category_id: 5, image: fastfood },
   { id: 9, label: '편의점', category_id: 4, image: convenience },
   { id: 10, label: '카페', category_id: 6, image: cafe },
-  { id: 11, label: '기타', category_id: 2, image: etc },
+  { id: 11, label: '선한 영향력', category_id: 11, image: etc },
+  { id: 12, label: '기타', category_id: 2, image: etc },
 ]
 
 export default Filter
